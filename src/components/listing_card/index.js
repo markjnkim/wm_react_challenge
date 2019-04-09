0a9769720dde2b3590083d1d224280a51e6dd2f2
markjnkim <mark.jn.kim@gmail.com>
2019-04-08 18:33:04 -0700

    format jsx

--- src/components/listing_card/index.js
+++ src/components/listing_card/index.js
@@ -19,24 +19,24 @@ import {
 } from "../styles";
 
 const ListingCard = props => (
-  <CardWrapper onClick={()=> props.selectList(props.listing)}>
+  <CardWrapper onClick={() => props.selectList(props.listing)}>
     <AvatarWrapper>
       <Avatar img={`${get(props.listing, "avatar_image.small_url")}`} />
     </AvatarWrapper>
     <DivWrapper>
       <DIV2> {props.listing.city}, {props.listing.state} |  {Math.floor(props.listing.distance)}mi <SPAN>{isOpen(props.listing, new Date())}</SPAN></DIV2>
       <DIV1> {props.listing.name} </DIV1>
-      <DIV3> 
-      <StarRating width={starWidth(props.listing.rating)} />
+      <DIV3>
+        <StarRating width={starWidth(props.listing.rating)} />
         <SpanRating>{props.listing.rating.toFixed(1)}</SpanRating>
-       </DIV3>
-       <DIVO>{isOpen(props.listing, new Date())}</DIVO>
+      </DIV3>
+      <DIVO>{isOpen(props.listing, new Date())}</DIVO>
     </DivWrapper>
   </CardWrapper>
 );
 
 const mapStateToProps = state => {
-  console.log( "MSTP LC: ", state);
+  console.log("MSTP LC: ", state);
   return { selectedDetail: state.selectedDetail };
 };
 
